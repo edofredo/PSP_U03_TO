47e125781320be125a4851f28c8dae2dff342f0b
cristian <cristianademed@hotmail.com>
2021-11-22 09:17:58 +0100

    Commit inicial

---
+++ src/util/Operacion.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Operacion {
+    
+    private String operacion;
+    private int cantidad;
+    private Date fecha;
+
+    public Operacion(String op, int cant){
+        operacion = op;
+        cantidad = cantidad;
+        fecha = new Date();
+    }     
+    
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+          
+    
+    
+}
